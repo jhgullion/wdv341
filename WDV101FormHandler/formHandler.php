d0288cf69386d9dab62561edb493557b0b61ea07
Jeff Gullion <jhgullion@dmacc.edu>
2024-02-06 23:47:02 -0600

    move form handler example code to remote

---
+++ WDV101FormHandler/formHandler.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>WDV101 Basic Form Handler Example</title>
+</head>
+
+<body>
+<h1>WDV101 Intro HTML and CSS</h1>
+<h2>UNIT 3 Forms - Lesson 2 Server Side Processes</h2>
+<p>This page will demonstrate how a server side application will take the data that was entered on a form and display it within an HTML table. This example will work for any form. It is setup to read any or all fields on a form without needing any changes.  Other applications are more specific to the form they process and require updates anytime the form is changed.</p>
+
+<h3>Instructions</h3>
+
+<p>The table below displays the 'name=value' pairs that were entered on the form and processed on the server.  This page is a result of that server side process.</p>
+<p>The <strong>Field Name</strong> column contains the value of the name attribute for each field on the form. <em>Example: &lt;input name=&quot;first_name&quot;&gt;</em>  This displays what you coded into the HTML. NOTE: If you do not have a name attribute for a field OR if the name attribute does not have a value the form will NOT send the data to the server.</p>
+<p>The <strong>Value of Field</strong> column contains the value of each field that was sent to the server by the form. This will vary depending upon the HTML form element and how the value attribute was used for a field.</p>
+<h3>Form Name-Value Pairs</h3>
+<?php
+
+	echo "<table border='1'>";
+	echo "<tr><th>Field Name</th><th>Value of Field</th></tr>";
+	foreach($_POST as $key => $value)
+	{
+		echo '<tr>';
+		echo '<td>',$key,'</td>';
+		echo '<td>',$value,'</td>';
+		echo "</tr>";
+	} 
+	echo "</table>";
+	echo "<p>&nbsp;</p>";
+
+?>
+
+</body>
+</html>
